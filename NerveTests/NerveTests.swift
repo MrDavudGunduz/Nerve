72ca8b558252d74f799f3c96fef05c7c526b12da
MrDavudGunduz <151569606+MrDavudGunduz@users.noreply.github.com>
2026-03-25 14:13:32 +0300

    chore: Initialize Map & AR based news project
    - Set up base Xcode project targeting iOS, macOS, and visionOS.

---
+++ NerveTests/NerveTests.swift
@@ -0,0 +1,16 @@
+//
+//  NerveTests.swift
+//  NerveTests
+//
+//  Created by Davud Gunduz on 25.03.2026.
+//
+
+import Testing
+
+struct NerveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
